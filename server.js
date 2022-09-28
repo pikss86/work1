3fac8090b2c17cb25004006027060d7e10e844b3
Pavel Ermolaev <pikss9123@gmail.com>
2022-09-28 11:04:31 +0300

    WIP

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('hello');
+    res.end();
+});
+
+server.listen(8080);
