d65d47eca430f8cf24b3d6e3d6456c80688822df
Pavel Ermolaev <pikss86@gmail.com>
2022-09-29 15:35:02 +0600

    save invoices in mem

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 import Fastify from 'fastify'
+import { readFile } from 'fs/promises';
 
-var counter = 0;
+const port = 8080
+const host = '0.0.0.0'
+const invoices = []
+const demo_items = JSON.parse(await readFile('./demo_items.json'))
 
 const fastify = Fastify({
     logger: true
 })
 
-fastify.all('/callback', function (request, reply) {
-    counter++;
-    reply.send(counter);
-})
-
 fastify.get('/', function (request, reply) {
-    reply.send(counter);
+    reply.redirect('/invoices')
 })
 
-fastify.listen({ port: 8080, host: '0.0.0.0' }, function (err, address) {
+fastify.post('/callback', function (request, reply) {
+    invoices.push(request.body)
+    reply.send({ success: true })
+})
+
+fastify.get('/invoices', function (request, reply) {
+    reply.send(invoices)
+})
+
+fastify.get('/items', function (request, reply) {
+    reply.send(demo_items)
+})
+
+fastify.listen({ port, host }, function (err, address) {
     if (err) {
         fastify.log.error(err)
         process.exit(1)
